f0189dcd623655ad2084a60ba0d1fcaeef7a90df
YaelRoeten <yaelroetenn@gmail.com>
2025-08-25 15:27:48 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    checkpoint_gehaald = 1
+    basic.pause(500)
+    checkpoint_gehaald = 0
+})
+let timer = 0
+let checkpoint_gehaald = 0
 let verbinding = randint(0, 255)
 radio.setGroup(verbinding)
-let timer = 0
 basic.pause(5000)
 basic.forever(function () {
+    if (checkpoint_gehaald == 1) {
+        basic.showNumber(timer)
+    }
     basic.pause(100)
-    timer += timer + 0.1
+    timer += 0.1
 })
