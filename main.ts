2bcca594a19c6f9ca63e93c06e0826e139a620c5
YaelRoeten <yaelroetenn@gmail.com>
2025-08-25 16:24:26 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    checkpoint_gehaald = 1
-    basic.pause(500)
-    checkpoint_gehaald = 0
+    if (receivedNumber == 90) {
+        radio.setGroup(208)
+    }
+    if (receivedNumber == 1000) {
+        checkpoint_gehaald = 1
+        basic.pause(500)
+        checkpoint_gehaald = 0
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    start = 1
+    radio.setGroup(208)
+    radio.sendNumber(70)
+    while (start == 1) {
+        if (checkpoint_gehaald == 1) {
+            basic.showNumber(timer)
+        }
+        basic.pause(50)
+        timer += 0.05
+    }
 })
 let timer = 0
 let checkpoint_gehaald = 0
-let verbinding = randint(0, 255)
-radio.setGroup(verbinding)
-basic.pause(5000)
+let start = 0
+start = 0
+radio.setTransmitPower(7)
+radio.setGroup(25)
 basic.forever(function () {
-    if (checkpoint_gehaald == 1) {
-        basic.showNumber(timer)
-    }
-    basic.pause(50)
-    timer += 0.05
+	
 })
