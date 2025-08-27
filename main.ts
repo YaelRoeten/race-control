2aff74f3af4fb68e18c9c8eac19cadfcb17156c8
YaelRoeten <yaelroetenn@gmail.com>
2025-08-27 15:12:57 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
-input.onButtonPressed(Button.A, function () {
-    start = 1
-    radio.setGroup(208)
-    radio.sendNumber(70)
-    while (start == 1) {
-        let checkpoint_gehaald = 0
-        if (checkpoint_gehaald == 1) {
-            basic.showNumber(timer)
-        }
-        basic.pause(50)
-        timer += 0.05
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+    	
     }
 })
+input.onButtonPressed(Button.A, function () {
+    check_1 += timer
+    serial.redirectToUSB()
+    serial.writeValue("checkpoint_1", check_1)
+})
 input.onGesture(Gesture.Shake, function () {
-    timer = 0
-    start = 0
-    basic.showNumber(timer)
+	
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     serial.redirectToUSB()
@@ -41,11 +36,20 @@ input.onButtonPressed(Button.B, function () {
 })
 let last_timer = 0
 let timer_finish = 0
-let check_1 = 0
 let timer = 0
+let check_1 = 0
 let cheat_timer = 0
 let start = 0
-start = 0
 cheat_timer = 2
 radio.setTransmitPower(7)
 radio.setGroup(25)
+basic.forever(function () {
+    while (start == 1) {
+        let checkpoint_gehaald = 0
+        if (checkpoint_gehaald == 1) {
+            basic.showNumber(timer)
+        }
+        basic.pause(50)
+        timer += 0.05
+    }
+})
