41a33afb80000b552dcc8e5f051c5e6bf418da33
YaelRoeten <yaelroetenn@gmail.com>
2025-08-27 10:31:49 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -11,12 +11,25 @@ input.onButtonPressed(Button.A, function () {
         timer += 0.05
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    timer = 0
+    start = 0
+    basic.showNumber(timer)
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showNumber(timer)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 2; index++) {
+        timer_finish += timer
+        basic.showIcon(IconNames.Yes)
+        basic.showNumber(timer_finish)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     start = 0
 })
+let timer_finish = 0
 let timer = 0
 let start = 0
 start = 0
